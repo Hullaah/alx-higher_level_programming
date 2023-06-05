01e421b6624e57f95ea8117ecbf097b640f79771
hullaah <umaradelo1.247@gmail.com>
2023-06-05 14:06:13 +0100

    some changes

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
 from sys import exit
-os.write(2, bytes("and that piece of art is useful - Dora Korpar, 2015-10-19\n", "utf-8"))
+word = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+os.write(2, bytes(word, "utf-8"))
 exit(1)

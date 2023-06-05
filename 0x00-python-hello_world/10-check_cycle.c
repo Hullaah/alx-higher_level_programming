d8ee3808f28bf29f0663b99a5ebc9fea98777c88
hullaah <umaradelo1.247@gmail.com>
2023-06-05 13:49:19 +0100

    made some corrections

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 int check_cycle(listint_t *list)
 {
+        (void) list;
         return (0);
 }
--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+import os
 from sys import exit
-exit("and that piece of art is useful - Dora Korpar, 2015-10-19")
+os.write(2, bytes("and that piece of art is useful - Dora Korpar, 2015-10-19\n", "utf-8"))
+exit(1)

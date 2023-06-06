12a5e40c92884e1067ecb2cf71dfa6c203263973
hullaah <umaradelo1.247@gmail.com>
2023-06-06 18:10:57 +0100

    corrected wrong indentation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(9):
     for j in range(1, 10):
         print("{}{}".format(i, j + i), end="\n" if i == 8 else ", ")
-            if j + i == 9:
-                break
+        if j + i == 9:
+            break

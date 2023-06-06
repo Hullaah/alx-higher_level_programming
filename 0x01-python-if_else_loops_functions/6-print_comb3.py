08938800d48fc0873600805a9ba47631fa41fa97
hullaah <umaradelo1.247@gmail.com>
2023-06-06 17:52:47 +0100

    solved some tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
      for j in range(1, 10):
-             print(f"{i}{j + i}", end="\n" if i == 8 else ", ")
+             print("{}{}".format(i, j + i), end="\n" if i == 8 else ", ")
              if j + i == 9:
                      break

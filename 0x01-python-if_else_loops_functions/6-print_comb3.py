f0254343109e09f07d5f0dacf45e25e4f7e0b34d
hullaah <umaradelo1.247@gmail.com>
2023-06-06 17:03:00 +0100

    made some changes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for i in range(9):
      for j in range(1, 10):
              print(f"{i}{j + i}", end="\n" if i == 8 else ", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@ def uppercase(str):
     for char in str:
         if char.isalpha():
             if 65 <= ord(char) <= 90:
-            	pass
+                pass
             else:
-            	char = "{:c}".format(ord(char) - 97 + 65)
+                char = "{:c}".format(ord(char) - 97 + 65)
         print(char, end="")
     print()

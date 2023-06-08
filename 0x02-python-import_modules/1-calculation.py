c4324476009cc1897aa2bf01452cd9c931b945ac
hullaah <umaradelo1.247@gmail.com>
2023-06-08 13:06:48 +0100

    made some changes to task 0 and task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ b = 5
 
 
 def main():
-    print("10 + 5 = {}".format(add(10, 5)))
-    print("10 - 5 = {}".format(sub(10, 5)))
-    print("10 * 5 = {}".format(mul(10, 5)))
-    print("10 / 5 = {}".format(div(10, 5)))
+    print("{} + {} = {}".format(10, 5, add(10, 5)))
+    print("{} - {} = {}".format(10, 5, sub(10, 5)))
+    print("{} * {} = {}".format(10, 5, mul(10, 5)))
+    print("{} / {} = {}".format(10, 5, div(10, 5)))
 
 
 if __name__ == "__main__":

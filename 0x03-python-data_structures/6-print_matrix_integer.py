a75487e722044244f68cfa991eed079f264ba270
hullaah <umaradelo1.247@gmail.com>
2023-06-10 14:56:09 +0100

    corrected file 6 and 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for column in rows:
-            print("{}".format(column),
+            print("{:d}".format(column),
                   end="\n"if column == rows[len(rows) - 1] else " ")

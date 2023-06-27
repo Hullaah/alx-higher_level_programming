cb309253994f31fa9f031a2bf55545fe32c61a9f
hullaah <umaradelo1.247@gmail.com>
2023-06-27 15:30:34 +0100

    solved more tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,3 +1,9 @@
 #!/usr/bin/pyhton3
 def list_division(my_list_1, my_list_2, list_length):
-    pass
+    ret = []
+    try:
+        for i in range(list_length):
+            try:
+                
+    except IndexError:
+

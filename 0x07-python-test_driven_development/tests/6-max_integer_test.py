cdd1052fffcfa9703a3fb8fd3ca9e1a8e260df03
hullaah <umaradelo1.247@gmail.com>
2023-07-04 19:20:10 +0100

    solved the last compulsory task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,4 +3,16 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test 
+    def test_max_normal(self):
+        result = max_integer([1, 2, 3, 4 , 5])
+        self.assertEqual(result, 5)
+    def test_max_negative(self):
+        result = max_integer([-1, -2, -3, -4, -5, -6])
+        self.assertEqual(result, -1)
+    def test_empty(self):
+        result = max_integer([])
+        self.assertEqual(result, None)
+
+
+if __name__ == "__main__":
+    unittest.main()

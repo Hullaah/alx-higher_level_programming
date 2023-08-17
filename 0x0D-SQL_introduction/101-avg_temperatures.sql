89dbced648e9e1e8c0986fb1e5ed18b4eb09281d
hullaah <umaradelo1.247@gmail.com>
2023-08-17 15:16:03 +0100

    Fixed some naming errors

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- A script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 -- Displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_tmp
+SELECT city, AVG(value) avg_temp
 FROM temperatures GROUP BY city
-ORDER BY avg_tmp DESC;
+ORDER BY avg_temp DESC;
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- A script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT g.name, COUNT(sg.genre_id) number_of_shows FROM tv_genres g
+SELECT g.name genre, COUNT(sg.genre_id) number_of_shows FROM tv_genres g
 INNER JOIN tv_show_genres sg ON g.id = sg.genre_id
 GROUP BY g.name ORDER BY number_of_shows DESC;

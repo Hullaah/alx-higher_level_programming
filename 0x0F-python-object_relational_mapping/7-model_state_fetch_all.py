8e96b66fd772f014c9d658e1fdd9a7035c83c1c8
hullaah <umaradelo1.247@gmail.com>
2023-09-16 07:46:47 -0700

    Solved task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the
+database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+username, password, database = tuple(argv[1:])
+connection_string = f"mysql+mysqldb://{username}:{password}"
+connection_string += f"@localhost:3306/{database}"
+engine = create_engine(connection_string)
+
+with Session(engine) as session:
+    table = session.query(State).order_by(State.id)
+    for row in table:
+        print(f"{row.id}: {row.name}")

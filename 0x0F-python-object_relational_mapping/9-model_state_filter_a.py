c2340db8dccf6bf32dc022efcdd5fb50ccc0fd78
hullaah <umaradelo1.247@gmail.com>
2023-09-16 09:14:35 -0700

    solved task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the
+database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+def main():
+    """entry point of code
+    """
+    username, password, database = tuple(argv[1:])
+    connection_string = f"mysql+mysqldb://{username}:{password}"
+    connection_string += f"@localhost:3306/{database}"
+    engine = create_engine(connection_string)
+
+    with Session(engine) as session:
+        table = session.query(State).filter(
+            State.name.like("%a%")).order_by(State.id)
+        for row in table:
+            print(f"{row.id}: {row.name}")
+
+
+if __name__ == "__main__":
+    main()

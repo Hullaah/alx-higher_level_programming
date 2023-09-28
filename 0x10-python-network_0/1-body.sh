e31dff5fa236f5bf7143baef55e5fb5e179df012
Hullaah <umaradelo1.247@gmail.com>
2023-09-28 17:39:49 +0100

    fixed some errors

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-TEST="$(curl -s -X GET "$1" | grep 'HTTP/[0-9]\.[0-9] 200 OK')"
-if [ -n "$TEST" ]; then
-    curl -s -X GET "$1"
-fi
-
+curl -s -X GET -L 200 "$1" 
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-curl -s -X POST -d "subject=$SUBJECT&email=$EMAIL" "$1"
+curl -s -X POST -d "subject=I WILL ALWAYS BE HERE FOR PLD&email=test@gmail.com" "$1"

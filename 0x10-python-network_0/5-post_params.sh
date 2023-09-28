1af96e835344540b103063bcbd719f598e2d4195
Hullaah <umaradelo1.247@gmail.com>
2023-09-28 22:46:13 +0100

    fixed more errors

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST -d "subject=I WILL ALWAYS BE HERE FOR PLD&email=test@gmail.com" "$1"
+curl -s -X POST -d "subject=I will always be here for PLD&email=test@gmail.com" "$1"

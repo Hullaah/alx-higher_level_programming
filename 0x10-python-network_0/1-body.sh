b19752b706b4d21cbc67c206187d2919a9c9a8c2
Hullaah <umaradelo1.247@gmail.com>
2023-09-28 17:29:22 +0100

    modified file 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # A Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -X GET "$1"
+TEST="$(curl -s -X GET "$1" | grep 'HTTP/[0-9]\.[0-9] 200 OK')"
+if [ -n "$TEST" ]; then
+    curl -s -X GET "$1"
+fi
+
